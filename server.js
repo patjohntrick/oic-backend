e0cbcd7d913bb38f7d8030101d276ea1f879aea6
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-06 08:23:55 +0800

    modify start script

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
-const { urlencoded } = require("express");
+const cors = require('cors');
+require('dotenv').config();
+const { urlencoded } = require('express');
 
 // Model
-const User = require("./model/User");
-const MoneyDonation = require("./model/MoneyDonation");
-const OtherDonation = require("./model/OtherDonation");
-const Activities = require("./model/ActivitiesModel");
-const AdminModel = require("./model/AdminModel");
+const User = require('./model/User');
+const MoneyDonation = require('./model/MoneyDonation');
+const OtherDonation = require('./model/OtherDonation');
+const Activities = require('./model/ActivitiesModel');
+const AdminModel = require('./model/AdminModel');
 
 // Middleware
 app.use(cors());
@@ -17,18 +17,20 @@ app.use(express.json());
 app.use(urlencoded({ extended: false }));
 
 // Routes
-const UserRouter = require("./routes/UserRoutes");
-const MoneyDonationRouter = require("./routes/DonateRoutes/MoneyDonationRoutes");
-const OtherDonationRouter = require("./routes/DonateRoutes/OtherDonationRoutes");
-const ActivitiesRouter = require("./routes/Activities/ActivitiesRoutes");
-const AdminLoginRouter = require("./routes/AdminLogin/AdminRoutes");
+const UserRouter = require('./routes/UserRoutes');
+const MoneyDonationRouter = require('./routes/DonateRoutes/MoneyDonationRoutes');
+const OtherDonationRouter = require('./routes/DonateRoutes/OtherDonationRoutes');
+const ActivitiesRouter = require('./routes/Activities/ActivitiesRoutes');
+const AdminLoginRouter = require('./routes/AdminLogin/AdminRoutes');
 
 // Routes middleware
-app.use("/user", UserRouter);
-app.use("/donation", MoneyDonationRouter);
-app.use("/donation", OtherDonationRouter);
-app.use("/activity", ActivitiesRouter);
-app.use("/admin", AdminLoginRouter);
+app.use('/user', UserRouter);
+app.use('/donation', MoneyDonationRouter);
+app.use('/donation', OtherDonationRouter);
+app.use('/activity', ActivitiesRouter);
+app.use('/admin', AdminLoginRouter);
 
 const PORT = process.env.PORT || 5000;
+// const PORT = 'https://localhost:5000';
+// console.log(process.env.PORT);
 app.listen(PORT, () => console.log(`app is listening to port ${PORT}`));
